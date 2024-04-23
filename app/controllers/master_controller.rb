8a30a578d8bcfded69a8ce4cf38afd40b7b5b01c
felipeferla <164975883+felipeferla@users.noreply.github.com>
2024-04-23 15:12:03 +0000

    v2

--- app/controllers/master_controller.rb
+++ app/controllers/master_controller.rb
@@ -1,47 +1,45 @@
 class MasterController < ApplicationController
-
   def go_homepage
-    render({:template => "game_templates/homepage"})
+    render({ :template => "game_templates/homepage" })
   end
 
   def play_two_six
     @rolls = []
-    2.times do 
+    2.times do
       roll = rand(1..6)
       rolls.push(roll)
     end
-  
-    render({:template => "game_templates/two_six"})
+
+    render({ :template => "game_templates/two_six" })
   end
 
   def play_two_ten
     @rolls = []
-    2.times do 
+    2.times do
       roll = rand(1..10)
       rolls.push(roll)
     end
-  
-    render({:template => "game_templates/two_ten"})
+
+    render({ :template => "game_templates/two_ten" })
   end
 
-  def play_two_six
+  def play_one_twenty
     @rolls = []
-    2.times do 
-      roll = rand(1..6)
+    1.times do
+      roll = rand(1..20)
       rolls.push(roll)
     end
-  
-    render({:template => "game_templates/two_six"})
+
+    render({ :template => "game_templates/one_twenty" })
   end
 
-  def play_two_six
+  def play_five_four
     @rolls = []
-    2.times do 
-      roll = rand(1..6)
+    5.times do
+      roll = rand(1..4)
       rolls.push(roll)
     end
-  
-    render({:template => "game_templates/two_six"})
-  end
 
+    render({ :template => "game_templates/five_four" })
+  end
 end
