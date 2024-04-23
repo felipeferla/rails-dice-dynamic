71788f60b3ba3d2c947fef6f2ea20f9048d42c08
felipeferla <164975883+felipeferla@users.noreply.github.com>
2024-04-23 15:09:30 +0000

    v1

--- app/controllers/master_controller.rb
+++ app/controllers/master_controller.rb
@@ -4,5 +4,44 @@ class MasterController < ApplicationController
     render({:template => "game_templates/homepage"})
   end
 
+  def play_two_six
+    @rolls = []
+    2.times do 
+      roll = rand(1..6)
+      rolls.push(roll)
+    end
   
+    render({:template => "game_templates/two_six"})
+  end
+
+  def play_two_ten
+    @rolls = []
+    2.times do 
+      roll = rand(1..10)
+      rolls.push(roll)
+    end
+  
+    render({:template => "game_templates/two_ten"})
+  end
+
+  def play_two_six
+    @rolls = []
+    2.times do 
+      roll = rand(1..6)
+      rolls.push(roll)
+    end
+  
+    render({:template => "game_templates/two_six"})
+  end
+
+  def play_two_six
+    @rolls = []
+    2.times do 
+      roll = rand(1..6)
+      rolls.push(roll)
+    end
+  
+    render({:template => "game_templates/two_six"})
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   get("/", { :controller => "master", :action => "go_homepage" })
+
+  get("dice/2/6", { :controller => "master", :action => "play_two_six" })
+
+  get("dice/2/10", { :controller => "master", :action => "play_two_ten" })
+
+  get("dice/1/20", { :controller => "master", :action => "play_one_twenty" })
+
+  get("dice/5/4", { :controller => "master", :action => "play_five_four" })
+
 end
